fd98c6b62419a8cb5749412d7dda00417a6ed0dc
Mystogan <prinz.wirudan@gmail.com>
2022-06-22 17:24:50 +0700

    tambah galeri sukses

--- components/TambahGaleri.vue
+++ components/TambahGaleri.vue
@@ -19,7 +19,7 @@
                     @change.prevent="onFileChange1"
                   />
                   <label class="custom-file-label" for="inputGroupFile04"
-                    >Pilih Gambar</label
+                    >Pilih Gambar 1</label
                   >
                 </div>
               </div>
@@ -44,7 +44,7 @@
                     @change.prevent="onFileChange2"
                   />
                   <label class="custom-file-label" for="inputGroupFile04"
-                    >Pilih Gambar</label
+                    >Pilih Gambar 2</label
                   >
                 </div>
               </div>
@@ -69,7 +69,7 @@
                     @change.prevent="onFileChange3"
                   />
                   <label class="custom-file-label" for="inputGroupFile04"
-                    >Pilih Gambar</label
+                    >Pilih Gambar 3</label
                   >
                 </div>
               </div>
@@ -94,7 +94,7 @@
                     @change.prevent="onFileChange4"
                   />
                   <label class="custom-file-label" for="inputGroupFile04"
-                    >Pilih Gambar</label
+                    >Pilih Gambar 4</label
                   >
                 </div>
               </div>
@@ -110,7 +110,7 @@
             </div>
             <div class="col-md-12 mt-4 mb-4">
               <base-button type="info" class="btn-fill" @click="onSubmit">
-                Tambahkan
+                Upload Foto
               </base-button>
             </div>
             <ul class="navbar-nav ml-auto">
@@ -123,9 +123,7 @@
                 <div class="mx-auto">
                   <div class="row">
                     <div class="col-md-10">
-                      <p style="font-size: 25px">
-                        Artikel Berhasil Ditambahkan
-                      </p>
+                      <p style="font-size: 25px">Foto Berhasil Ditambahkan</p>
                     </div>
                     <div class="col-md-2">
                       <img
@@ -136,7 +134,7 @@
                     </div>
                   </div>
                   <div>
-                    Kembali ke halaman utama dalam
+                    Kembali ke halaman galeri dalam
                     {{ countDown }} detik
                   </div>
                 </div>
@@ -154,9 +152,17 @@
                 animated
               ></b-progress>
             </div>
+
+            <div class="col-md-12 mt-4 mb-4" v-show="tambahkan">
+              <base-button type="success" class="btn-fill" @click="onTambah">
+                Tambahkan
+              </base-button>
+            </div>
           </form>
-          <div class="col-md-12">
-            <h5>Note: Jumlah maksimal dalam upload gambar adalah 4 foto</h5>
+          <div class="col-md-12" v-show="tambahkan">
+            <h5>
+              Note: Tunggu upload file selesai, baru klik tombol tambahkan
+            </h5>
           </div>
         </div>
       </template>
@@ -186,6 +192,7 @@ export default {
         },
         theme: "snow",
       },
+      tambahkan: false,
       searchModalVisible: false,
       file_gambar1: null,
       file_gambar2: null,
@@ -277,102 +284,251 @@ export default {
 
     async onSubmit() {
       this.uploadSemua = true;
-      const file = [
-        this.file_gambar1,
-        this.file_gambar2,
-        this.file_gambar3,
-        this.file_gambar4,
-      ];
 
-      if (!file.type.match("image.*")) {
-        alert("Please upload an image.");
-        return;
-      }
-
-      const metadata = {
-        contentType: file.type,
-      };
-
-      this.isUploadingImage = true;
-
-      // Create a reference to the destination where we're uploading
-      // the file.
       let nowDate = moment().format("Do-MMMM-YYYY");
       let bulan_no = moment().format("MM");
       let bulan = moment().format("MMMM");
       let year = moment().format("YYYY");
-      const imageRef = storeFile
-        .ref(
-          `Galeri/${year}/Bulan|${bulan_no}|${bulan}/${nowDate}/${file.name}`
-        )
-        .put(file, metadata);
+      let rand = (
+        Math.random().toString(36).substring(2, 16) +
+        Math.random().toString(36).substring(2, 16)
+      ).toUpperCase();
 
-      imageRef.on(
-        `state_changed`,
-        (snapshot) => {
-          this.uploadValue =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        },
-        (error) => {
-          console.log(error.message);
-        },
-        () => {
-          this.uploadValue = 100;
-          imageRef.snapshot.ref.getDownloadURL().then((url) => {
-            let tanggalTerbit = moment().format("Do MMMM YYYY");
-            let jamTerbit = moment().format("H:mm:ss");
-            let nama_admin_cookie = Cookie.get("nama");
-            let jabatan_cookie = Cookie.get("jabatan");
-            let url_string = url.toString();
-            let url_imagekit = url_string.replace(
-              "https://firebasestorage.googleapis.com/v0/b/dashboard-school-5b47d.appspot.com",
-              "https://ik.imagekit.io/enterbiner/"
-            );
-
-            this.url = url_imagekit;
-            this.blog.imageUrl = url_imagekit;
-            this.form.nama_admin = nama_admin_cookie;
-            this.form.jabatan = jabatan_cookie;
-            this.form.tanggal_terbit = tanggalTerbit;
-            this.form.jam_terbit = jamTerbit;
-            this.form.gambar_artikel = url_imagekit;
-
-            this.url = url_imagekit;
-            this.blog.imageUrl = url_imagekit;
-            this.form.nama_admin = nama_admin_cookie;
-            this.form.jabatan = jabatan_cookie;
-            this.form.tanggal_terbit = tanggalTerbit;
-            this.form.jam_terbit = jamTerbit;
-            this.form.gambar_artikel = url_imagekit;
-
-            this.url = url_imagekit;
-            this.blog.imageUrl = url_imagekit;
-            this.form.nama_admin = nama_admin_cookie;
-            this.form.jabatan = jabatan_cookie;
-            this.form.tanggal_terbit = tanggalTerbit;
-            this.form.jam_terbit = jamTerbit;
-            this.form.gambar_artikel = url_imagekit;
-
-            this.url = url_imagekit;
-            this.blog.imageUrl = url_imagekit;
-            this.form.nama_admin = nama_admin_cookie;
-            this.form.jabatan = jabatan_cookie;
-            this.form.tanggal_terbit = tanggalTerbit;
-            this.form.jam_terbit = jamTerbit;
-            this.form.gambar_artikel = url_imagekit;
-
-            // this.$store.dispatch("addGaleri", this.formInfo);
-            this.isUploadingImage = false;
-            this.searchModalVisible = true;
-            this.countDownTimer();
-            this.interval = setTimeout(
-              function () {
-                this.$router.push({ path: "/artikel" });
-              }.bind(this),
-              5000
-            );
-          });
+      const file1 = this.file_gambar1;
+      const file2 = this.file_gambar2;
+      const file3 = this.file_gambar3;
+      const file4 = this.file_gambar4;
+      if (file1 == null) {
+        console.log("tidak ada foto untuk form ke-1");
+      } else {
+        if (!file1.type.match("image.*")) {
+          alert("Please upload an image.");
+          return;
         }
+        const metadata1 = {
+          contentType: file1.type,
+        };
+
+        this.isUploadingImage = true;
+
+        const imageRef1 = storeFile
+          .ref(
+            `Galeri/${year}/Bulan|${bulan_no}|${bulan}/${nowDate}/${file1.name}`
+          )
+          .put(file1, metadata1);
+
+        imageRef1.on(
+          `state_changed`,
+          (snapshot) => {
+            this.uploadValue =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          },
+          (error) => {
+            console.log(error.message);
+          },
+          () => {
+            this.uploadValue = 100;
+            imageRef1.snapshot.ref.getDownloadURL().then((url) => {
+              let tanggalTerbit = moment().format("Do MMMM YYYY");
+              let jamTerbit = moment().format("H:mm:ss");
+              let nama_admin_cookie = Cookie.get("nama");
+              let url_string = url.toString();
+              let url_imagekit = url_string.replace(
+                "https://firebasestorage.googleapis.com/v0/b/dashboard-school-5b47d.appspot.com",
+                "https://ik.imagekit.io/enterbiner/"
+              );
+
+              this.url1 = url_imagekit;
+              this.blog1.imageUrl = url_imagekit;
+              this.form1Galeri.nama_admin = nama_admin_cookie;
+              this.form1Galeri.tanggal_terbit = tanggalTerbit;
+              this.form1Galeri.jam_terbit = jamTerbit;
+              this.form1Galeri.galeri_foto = url_imagekit;
+              this.$store.dispatch("addGaleri", this.form1Galeri);
+              this.isUploadingImage = false;
+              this.tambahkan = true;
+            });
+          }
+        );
+      }
+
+      if (file2 == null) {
+        console.log("tidak ada foto untuk form ke-2");
+      } else {
+        if (!file2.type.match("image.*")) {
+          alert("Please upload an image.");
+          return;
+        }
+
+        const metadata2 = {
+          contentType: file2.type,
+        };
+
+        this.isUploadingImage = true;
+
+        const imageRef2 = storeFile
+          .ref(
+            `Galeri/${year}/Bulan|${bulan_no}|${bulan}/${nowDate}/${file2.name}`
+          )
+          .put(file2, metadata2);
+
+        imageRef2.on(
+          `state_changed`,
+          (snapshot) => {
+            this.uploadValue =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          },
+          (error) => {
+            console.log(error.message);
+          },
+          () => {
+            this.uploadValue = 100;
+            imageRef2.snapshot.ref.getDownloadURL().then((url) => {
+              let tanggalTerbit = moment().format("Do MMMM YYYY");
+              let jamTerbit = moment().format("H:mm:ss");
+              let nama_admin_cookie = Cookie.get("nama");
+              let url_string = url.toString();
+              let url_imagekit = url_string.replace(
+                "https://firebasestorage.googleapis.com/v0/b/dashboard-school-5b47d.appspot.com",
+                "https://ik.imagekit.io/enterbiner/"
+              );
+
+              this.url2 = url_imagekit;
+              this.blog2.imageUrl = url_imagekit;
+              this.form2Galeri.nama_admin = nama_admin_cookie;
+              this.form2Galeri.tanggal_terbit = tanggalTerbit;
+              this.form2Galeri.jam_terbit = jamTerbit;
+              this.form2Galeri.galeri_foto = url_imagekit;
+              this.$store.dispatch("addGaleri", this.form2Galeri);
+              this.isUploadingImage = false;
+              this.tambahkan = true;
+            });
+          }
+        );
+      }
+
+      if (file3 == null) {
+        console.log("tidak ada foto untuk form ke-3");
+      } else {
+        if (!file3.type.match("image.*")) {
+          alert("Please upload an image.");
+          return;
+        }
+
+        const metadata3 = {
+          contentType: file3.type,
+        };
+
+        this.isUploadingImage = true;
+
+        const imageRef3 = storeFile
+          .ref(
+            `Galeri/${year}/Bulan|${bulan_no}|${bulan}/${nowDate}/${file3.name}`
+          )
+          .put(file3, metadata3);
+
+        imageRef3.on(
+          `state_changed`,
+          (snapshot) => {
+            this.uploadValue =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          },
+          (error) => {
+            console.log(error.message);
+          },
+          () => {
+            this.uploadValue = 100;
+            imageRef3.snapshot.ref.getDownloadURL().then((url) => {
+              let tanggalTerbit = moment().format("Do MMMM YYYY");
+              let jamTerbit = moment().format("H:mm:ss");
+              let nama_admin_cookie = Cookie.get("nama");
+              let url_string = url.toString();
+              let url_imagekit = url_string.replace(
+                "https://firebasestorage.googleapis.com/v0/b/dashboard-school-5b47d.appspot.com",
+                "https://ik.imagekit.io/enterbiner/"
+              );
+
+              this.url3 = url_imagekit;
+              this.blog3.imageUrl = url_imagekit;
+              this.form3Galeri.nama_admin = nama_admin_cookie;
+              this.form3Galeri.tanggal_terbit = tanggalTerbit;
+              this.form3Galeri.jam_terbit = jamTerbit;
+              this.form3Galeri.galeri_foto = url_imagekit;
+              this.$store.dispatch("addGaleri", this.form3Galeri);
+              this.isUploadingImage = false;
+              this.tambahkan = true;
+            });
+          }
+        );
+      }
+
+      if (file4 == null) {
+        console.log("tidak ada foto untuk form ke-4");
+      } else {
+        if (!file4.type.match("image.*")) {
+          alert("Please upload an image.");
+          return;
+        }
+
+        const metadata4 = {
+          contentType: file4.type,
+        };
+
+        this.isUploadingImage = true;
+
+        const imageRef4 = storeFile
+          .ref(
+            `Galeri/${year}/Bulan|${bulan_no}|${bulan}/${nowDate}/${file4.name}`
+          )
+          .put(file4, metadata4);
+
+        imageRef4.on(
+          `state_changed`,
+          (snapshot) => {
+            this.uploadValue =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          },
+          (error) => {
+            console.log(error.message);
+          },
+          () => {
+            this.uploadValue = 100;
+            imageRef4.snapshot.ref.getDownloadURL().then((url) => {
+              let tanggalTerbit = moment().format("Do MMMM YYYY");
+              let jamTerbit = moment().format("H:mm:ss");
+              let nama_admin_cookie = Cookie.get("nama");
+              let url_string = url.toString();
+              let url_imagekit = url_string.replace(
+                "https://firebasestorage.googleapis.com/v0/b/dashboard-school-5b47d.appspot.com",
+                "https://ik.imagekit.io/enterbiner/"
+              );
+
+              this.url4 = url_imagekit;
+              this.blog4.imageUrl = url_imagekit;
+              this.form4Galeri.nama_admin = nama_admin_cookie;
+              this.form4Galeri.tanggal_terbit = tanggalTerbit;
+              this.form4Galeri.jam_terbit = jamTerbit;
+              this.form4Galeri.galeri_foto = url_imagekit;
+              this.$store.dispatch("addGaleri", this.form4Galeri);
+              this.isUploadingImage = false;
+              this.tambahkan = true;
+            });
+          }
+        );
+      }
+
+      // Create a reference to the destination where we're uploading
+      // the file.
+    },
+
+    async onTambah() {
+      this.searchModalVisible = true;
+      this.countDownTimer();
+      this.interval = setTimeout(
+        function () {
+          this.$router.push({ path: "/superadmin/galeri" }).then($router.go(0));
+        }.bind(this),
+        5000
       );
     },
   },
